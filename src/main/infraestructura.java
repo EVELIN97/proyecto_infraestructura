16aa3a7d8c5d5d4aa2a38297f4d097f142973fb3
EVELIN97 <evlyn.mas@gmail.com>
2021-06-12 19:39:33 -0400

    iniciando proyecto infraestructura

---
+++ src/main/infraestructura.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Evelin
+ */
+public class infraestructura {
+    public String Nombre;
+    public int m2;
+
+    public infraestructura(String Nombre, int m2) {
+        this.Nombre = Nombre;
+        this.m2 = m2;
+    }
+    
+}
